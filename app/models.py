a42c64d1f5399d27b69949bf25fad77cfc4483bd
HSLoveZL <hjmrezl@outlook.com>
2016-12-20 23:36:58 +0800

    modify,add models User, Roles

---
+++ app/models.py
@@ -0,0 +1,51 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask import Flask
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+	
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+	'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+
+
+db = SQLAlchemy(app)
+
+
+class Role(db.Model):
+	__tablename__ = 'roles'
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(64), unique=True)
+
+	def __repr__(self):
+		return '<Role %r>' % self.name 
+
+	users = db.relationship('User', backref='role')
+
+
+class User(db.Model):
+	__tablename__ = 'users'
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(64), unique=True, index=True)
+
+	def __repr__(self):
+		return '<User %r>' % self.username
+
+	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+	password_hash = db.Column(db.String(128))
+
+	@property
+	def password(self):
+		raise AttributeError('password is not a readerable attribute')
+
+	@password.setter
+	def password(self, password):
+		self.password_hash = generate_password_hash(password)
+
+	def verify_password(self, password):
+		return check_password_hash(self.password_hash, password)
