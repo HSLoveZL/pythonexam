e4bfa9a259c575d95236e28cc7254b168ce21d7d
HSLoveZL <hjmrezl@outlook.com>
2016-12-21 14:12:38 +0800

    config files

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask.ext.bootstrap import Bootstrap
 from flask.ext.mail import Mail
 from flask.ext.moment import Moment
 from flask.ext.sqlalchemy import SQLAlchemy
+from config import config
 
 bootstrap = Bootstrap()
 mail = Mail()
